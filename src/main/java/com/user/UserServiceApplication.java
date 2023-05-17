a24e6416d0a7dde9c81a363fe73135675d3aaa86
VinitPania <vinit.pania@cloverinfotech.com>
2023-05-17 16:43:27 +0530

    Changed pom and main so that it can generate war

--- src/main/java/com/user/UserServiceApplication.java
+++ src/main/java/com/user/UserServiceApplication.java
@@ -1,5 +1,7 @@
 package com.user;
 
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
@@ -7,11 +9,16 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
-public class UserServiceApplication {
+public class UserServiceApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(UserServiceApplication.class, args);
     }
+    
+    @Override
+      protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+              return application.sources(UserServiceApplication.class); 
+      }
 
 
     @Bean
